6f683e8f86393a35af94d653b2535e17c6276572
Husanboy Zafarov <zhzr1222@gmail.com>
2024-09-09 21:40:24 +0500

    yangilash va vue3 kursini boshlash, v-on joyiga yetdim

---
+++ Vue-Vite-Creation/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="m-4">
+        <h1>Counter: {{ counter }}</h1>
+        <div class="d-flex gap-2">
+            <button @click="onIncrement" class="btn btn-outline-success">Add</button>
+            <button @click="onDecrement" class="btn btn-outline-danger">Delete</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+        }
+
+    },
+    methods: {
+        onIncrement() {
+            this.counter += 1
+        },
+        onDecrement() {
+            this.counter -= 1
+        },
+    }
+}
+</script>
+
+<style></style>
